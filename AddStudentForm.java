ec7bd100645a4d6684bf93170b616809087299e7
Meno <menolycasch2006@gmail.com>
2026-07-15 19:33:34 +0530

    Commit 6

--- AddStudentForm.java
+++ AddStudentForm.java
@@ -11,7 +11,9 @@ class AddStudentForm  extends JFrame{
 	private JButton btAdd;
 	
 	private StudentList studentList;
+
 	AddStudentForm(StudentList studentList){
+
 		setSize(400,300);
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
@@ -20,3 +22,31 @@ class AddStudentForm  extends JFrame{
 		titleLabel.setFont(new Font("",1,27));
 		titleLabel.setHorizontalAlignment(JLabel.CENTER);
 		add("North",titleLabel);
+
+	// ------------------------------------------------
+
+
+	JPanel southPanel=new JPanel(new FlowLayout(FlowLayout.RIGHT));
+		btAdd=new JButton("Add Student");
+		btCancel=new JButton("Cancel");
+		btAdd.setFont(new Font("",1,20));
+		btCancel.setFont(new Font("",1,20));
+		
+		southPanel.add(btAdd);
+		btAdd.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent evt){
+				
+				String id=txtStudentId.getText();
+				String name=txtName.getText();
+				int prfMarks=Integer.parseInt(txtPrfMarks.getText());
+				int dbmsMarks=Integer.parseInt(txtDbmsMarks.getText());
+				Student s1=new Student(id,name,prfMarks,dbmsMarks);
+				studentList.add(s1);
+			}
+		});
+		
+		southPanel.add(btCancel);
+		add("South",southPanel);
+
+
+		
