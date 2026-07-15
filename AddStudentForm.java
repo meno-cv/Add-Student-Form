f73dbc8ef3baafbee23f001207eafafb8426ca05
Meno <menolycasch2006@gmail.com>
2026-07-15 19:38:21 +0530

    Commit 7

--- AddStudentForm.java
+++ AddStudentForm.java
@@ -35,7 +35,7 @@ class AddStudentForm  extends JFrame{
 		southPanel.add(btAdd);
 		btAdd.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent evt){
-				
+
 				String id=txtStudentId.getText();
 				String name=txtName.getText();
 				int prfMarks=Integer.parseInt(txtPrfMarks.getText());
@@ -47,6 +47,29 @@ class AddStudentForm  extends JFrame{
 		
 		southPanel.add(btCancel);
 		add("South",southPanel);
+
+		//-------------------------------------------------------------
+		
+		JLabel lblStudentId=new JLabel("Student ID");
+		JLabel lblName=new JLabel("Name");
+		JLabel lblPrfMarks=new JLabel("Prf Marks");
+		JLabel lblDbmsMarks=new JLabel("Dbms Marks");
+		lblStudentId.setFont(new Font("",1,20));
+		lblName.setFont(new Font("",1,20));
+		lblPrfMarks.setFont(new Font("",1,20));
+		lblDbmsMarks.setFont(new Font("",1,20));
+		
+		JPanel labelPanel=new JPanel(new GridLayout(4,1));
+		JPanel idTextPanal=new JPanel(new FlowLayout(FlowLayout.LEFT));
+
+		labelPanel.add(lblStudentId);
+		labelPanel.add(lblName);
+		labelPanel.add(lblPrfMarks);
+		labelPanel.add(lblDbmsMarks);
+		
+		add("West",labelPanel);
+		
+		//-------------------------------------------------------------
 
 
 		
