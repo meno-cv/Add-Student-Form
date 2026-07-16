1c775768f58aeb4c111657abdafaf48fa1cc7d4a
Meno <menolycasch2006@gmail.com>
2026-07-16 13:24:40 +0530

    Commit 11

--- UpdateStudentForm.java
+++ UpdateStudentForm.java
@@ -12,4 +12,12 @@ class SearchStudentForm  extends JFrame{
 	private JButton btCancel;
 	private JButton btSearch;
 
-    
+    SearchStudentForm(){
+		setSize(400,300);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		
+		JLabel titleLabel=new JLabel("Search Student Form");
+		titleLabel.setFont(new Font("",1,27));
+		titleLabel.setHorizontalAlignment(JLabel.CENTER);
+		add("North",titleLabel);
