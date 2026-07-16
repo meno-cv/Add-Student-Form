239a836825050af304e7935d1df30d73c37db91a
Meno <menolycasch2006@gmail.com>
2026-07-16 13:35:00 +0530

    Commit 12

--- UpdateStudentForm.java
+++ UpdateStudentForm.java
@@ -21,3 +21,35 @@ class SearchStudentForm  extends JFrame{
 		titleLabel.setFont(new Font("",1,27));
 		titleLabel.setHorizontalAlignment(JLabel.CENTER);
 		add("North",titleLabel);
+
+        		//-------------------------------------------------------------
+		
+		JPanel southPanel=new JPanel(new FlowLayout(FlowLayout.RIGHT));
+		btSearch=new JButton("Search Student");
+		btCancel=new JButton("Cancel");
+		btSearch.setFont(new Font("",1,20));
+		btSearch.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent evt){
+				try{
+					Scanner input=new Scanner(new File("Student.txt"));
+					String id=txtStudentId.getText();
+					String[] rowData=null;
+					while(input.hasNext()){
+						String line=input.nextLine();
+						rowData=line.split(",");
+						if(rowData[0].equalsIgnoreCase(id)){
+							break;
+						}
+					}
+					txtName.setText(rowData[1]);
+					txtPrfMarks.setText(rowData[2]);
+					txtDbmsMarks.setText(rowData[3]);
+				}catch(IOException ex){}
+				
+			}
+		});
+		btCancel.setFont(new Font("",1,20));
+		
+		southPanel.add(btSearch);
+		southPanel.add(btCancel);
+		add("South",southPanel);
