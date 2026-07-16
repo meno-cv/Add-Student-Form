78b6c2d3e7330170def381847e0810c697b9fd11
Meno <menolycasch2006@gmail.com>
2026-07-16 13:24:07 +0530

    Commit 10

---
+++ UpdateStudentForm.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+import java.util.*;
+import java.io.*;
+import java.awt.*;
+import java.awt.event.*;
+class SearchStudentForm  extends JFrame{
+	private JTextField txtStudentId;
+	private JTextField txtName;
+	private JTextField txtPrfMarks;
+	private JTextField txtDbmsMarks;
+	
+	private JButton btCancel;
+	private JButton btSearch;
+
+    
