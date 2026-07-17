8dfce60026026b28a67cd8b66ac2dc6304f554b8
Meno <menolycasch2006@gmail.com>
2026-07-17 23:12:27 +0530

    Commit 21 : Implement search functionality for student records in Search Student Form

--- SearchStudentForm.java
+++ SearchStudentForm.java
@@ -11,3 +11,30 @@ class SearchStudentForm  extends JFrame{
 	
 	private JButton btCancel;
 	private JButton btSearch;
+
+    //-------------------------------------------------------------
+		
+		JPanel southPanel=new JPanel(new FlowLayout(FlowLayout.RIGHT));
+		btSearch=new JButton("Search Student");
+		btCancel=new JButton("Cancel");
+		btSearch.setFont(new Font("",1,20));
+		btSearch.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent evt){
+				try{
+					Scanner input=new Scanner(new File("Student.txt"));
+					String id=txtStudentId.getText();
+					String[] rowData=null;
+					while(input.hasNext()){
+						String line=input.nextLine();
+						rowData=line.split(",");
+						if(rowData[0].equalsIgnoreCase(id)){
+							break;
+						}
+					}
+					txtName.setText(rowData[1]);
+					txtPrfMarks.setText(rowData[2]);
+					txtDbmsMarks.setText(rowData[3]);
+				}catch(IOException ex){}
+				
+			}
+		});
