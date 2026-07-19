42bac72b159d22e469d442d70d090709be1158d6
Meno <menolycasch2006@gmail.com>
2026-07-19 10:24:04 +0530

    Commit 22 : Enhance student search functionality to display student details or show a message if not found

--- SearchStudentForm.java
+++ SearchStudentForm.java
@@ -1,40 +1,85 @@
-import javax.swing.*;
-import java.util.*;
-import java.io.*;
-import java.awt.*;
-import java.awt.event.*;
-class SearchStudentForm  extends JFrame{
-	private JTextField txtStudentId;
-	private JTextField txtName;
-	private JTextField txtPrfMarks;
-	private JTextField txtDbmsMarks;
-	
-	private JButton btCancel;
-	private JButton btSearch;
+package studentmanagementsystem;
 
-    //-------------------------------------------------------------
-		
-		JPanel southPanel=new JPanel(new FlowLayout(FlowLayout.RIGHT));
-		btSearch=new JButton("Search Student");
-		btCancel=new JButton("Cancel");
-		btSearch.setFont(new Font("",1,20));
-		btSearch.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent evt){
-				try{
-					Scanner input=new Scanner(new File("Student.txt"));
-					String id=txtStudentId.getText();
-					String[] rowData=null;
-					while(input.hasNext()){
-						String line=input.nextLine();
-						rowData=line.split(",");
-						if(rowData[0].equalsIgnoreCase(id)){
-							break;
-						}
-					}
-					txtName.setText(rowData[1]);
-					txtPrfMarks.setText(rowData[2]);
-					txtDbmsMarks.setText(rowData[3]);
-				}catch(IOException ex){}
-				
-			}
-		});
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+
+public class SearchStudentForm extends javax.swing.JFrame {
+
+    /**
+     * Creates new form AddStudentForm
+     */
+    public SearchStudentForm() {
+        initComponents();
+    }
+
+    private void btnCancelActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCancelActionPerformed
+        dispose();
+    }//GEN-LAST:event_btnCancelActionPerformed
+
+    private void btnSearchActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSearchActionPerformed
+    }//GEN-LAST:event_btnSearchActionPerformed
+
+    private void txtIdActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtIdActionPerformed
+        try {
+            Scanner input=new Scanner(new File("Student.txt"));
+            String id=txtId.getText();
+            String row=null;
+            String[] rowData=null;
+            while(input.hasNext()){
+                row=input.nextLine();
+                rowData=row.split(",");
+                if(rowData[0].equalsIgnoreCase(id)){
+                    break;
+                }
+                row=null;
+            }
+            if(row!=null){
+                txtName.setText(rowData[1]);
+                txtPrfMarks.setText(rowData[2]);
+                txtDbmsMarks.setText(rowData[3]);
+            }else{
+                JOptionPane.showMessageDialog(this,"Student not found..");
+            }
+            
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(SearchStudentForm.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }//GEN-LAST:event_txtIdActionPerformed
+
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(SearchStudentForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(SearchStudentForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(SearchStudentForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(SearchStudentForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+       
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new SearchStudentForm().setVisible(true);
+            }
+        });
+    }
