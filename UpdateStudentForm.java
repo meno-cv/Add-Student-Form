56d6472f15898c681cffc670cbc7b77452a4e31a
Meno <menolycasch2006@gmail.com>
2026-07-19 10:28:51 +0530

    Commit 24 : Enhance UpdateStudentForm layout and functionality with improved UI components

--- UpdateStudentForm.java
+++ UpdateStudentForm.java
@@ -31,4 +31,43 @@ public class UpdateStudentForm extends javax.swing.JFrame {
         btnUpdate = new javax.swing.JButton();
         btnCancel = new javax.swing.JButton();
 
+		setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jLabel1.setFont(new java.awt.Font("Segoe UI", 1, 24)); // NOI18N
+        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel1.setText("Update Student Form");
+
+        jLabel2.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        jLabel2.setText("Student ID");
+
+        txtId.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        txtId.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txtIdActionPerformed(evt);
+            }
+        });
+
+        txtName.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+
+        jLabel3.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        jLabel3.setText("Name");
+
+        jLabel4.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        jLabel4.setText("Marks PRF");
+
+        txtPrfMarks.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+
+        jLabel5.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        jLabel5.setText("Marks DBMS");
+
+        txtDbmsMarks.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+
+        btnUpdate.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        btnUpdate.setText("Update");
+        btnUpdate.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnUpdateActionPerformed(evt);
+            }
+        });
+
        
